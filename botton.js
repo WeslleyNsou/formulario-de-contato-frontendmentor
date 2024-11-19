e8fd55f4fd30bf5ba8009508dd446192448658b7
Weslley souza <120567284+WeslleyNsou@users.noreply.github.com>
2024-11-19 07:40:01 -0300

    inicio do projeto

---
+++ botton.js
@@ -0,0 +1,8 @@
+function enviar(){
+    if(Input.length.value == 0){
+        alert("preencha os campos obrigatoris!")
+    }else{
+        alert("Thanks for completing the form. We'll be in touch soon!")
+
+    }
+}
