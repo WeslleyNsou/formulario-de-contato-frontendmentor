a5d3e3d44c693755ca5af8d1ee393e013ea864a3
Weslley souza <120567284+WeslleyNsou@users.noreply.github.com>
2024-11-27 07:13:21 -0300

    restart

--- botton.js
+++
@@ -1,8 +0,0 @@
-function enviar(){
-    if(Input.length.value == 0){
-        alert("preencha os campos obrigatoris!")
-    }else{
-        alert("Thanks for completing the form. We'll be in touch soon!")
-
-    }
-}
